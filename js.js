6b57c217346cb827b773727b0bab0c837ed6e5c8
TaylorEisman12 <taylor@theeismans.com>
2012-03-04 16:15:53 -0600

    Started my Js file and added getElementById, createCats, and a getRadioData function.

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
-Javascript
+
+window.addEventListener("DOMContentLoader", function(){
+  function $(){
+		var carInfo = document.getElementById(info);
+		return carInfo;
+	}
+		function createCats(){
+			var formTag = document.getElementsByTagName("form"),
+			;
+		}
+	}
+	function getRadioData(){
+		var radio = document.form[0].color;
+			for(var i=0; i<radio.length; i++){
+				if(radio[i].checked){
+					colorChosen = radio[i].value;
+			}
+		};
+	
